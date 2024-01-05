cfd72c01b19a43798aca6c577359b4a88bbe6462
Biruk Habte <biruk.habte-ug@aau.edu.et>
2024-01-06 01:57:00 +0300

    Improve task error handling

--- Task_Backend/src/middlewares/authMiddleware.js
+++ Task_Backend/src/middlewares/authMiddleware.js
@@ -32,3 +32,4 @@ module.exports = authMiddleware;// Updated on 2024-01-17 10:34:00
 // Updated on 2024-01-05 01:23:00
 // Updated on 2024-01-26 04:25:00
 // Updated on 2024-01-31 07:34:00
+// Updated on 2024-01-06 01:57:00
